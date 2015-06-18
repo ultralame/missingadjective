7ff0045483d4cf09fa3ab61761b3e51f3b131b8a
Hack Reactor Students <accounts@hackreactor.com>
2015-06-18 16:38:01 -0700

    Implemented rooms for socket.io

--- server/server.js
+++ server/server.js
@@ -1,3 +1,6 @@
+// Helper Functions
+var helpers = require('./helpers.js');
+
 var express = require('express')
 var app = express();
 var server = require('http').Server(app);
@@ -12,6 +15,47 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../client'));
 
+var roomId = 0;
+var roomCounter = 0;
+var roomSize = 3;
 
+var leaveRoomQ = new helpers.Queue();
+
+io.on('connection', function(socket) {
+
+  socket.on('join', function(name) {
+
+    socket.name = name;
+    socket.room = roomId;
+    socket.join(roomId);
+
+    if(leaveRoomQ.size() >= 1) {
+      socket.room = leaveRoomQ.dequeue();
+    }
+    else {
+      roomCounter++;
+      if(roomCounter === roomSize) {
+        roomId++;
+        roomCounter = 0;
+      }
+    }
+
+    console.log(socket.name + " has joined room " + socket.room + '.');
+  });
+
+
+  socket.on('disconnect',function() {
+
+    console.log(socket.name + " has left room " + socket.room + '.');
+
+    // maybe broadcast that the person has left the room
+    socket.leave(socket.room);
+
+    if(socket.room !== undefined) {
+      leaveRoomQ.enqueue(socket.room);
+    }
+  });
+
+});
 
 module.exports = server;
