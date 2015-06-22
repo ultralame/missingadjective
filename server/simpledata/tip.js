88fed0e16ec7b3837426b7c00b3669f11481547f
Shanan Sussman <gfxbox@gmail.com>
2015-06-22 12:30:03 -0700

    removed data files for yelp data that we are no longer using

--- server/simpledata/tip.js
+++
@@ -1,9 +0,0 @@
-// basic Tip data entry
-var tip = {"user_id": "xuIeFhoebXnwnO41wu1UXg", "text": "Really cool new hotel with lots going on. Bars and restaurants bursting with energy. Book a strip view if you can sleep thru anything!", "business_id": "AtjsjFzalWqJ7S9DUFQ4bw", "likes": 0, "date": "2011-05-01", "type": "tip"};
-
-// storing all keys of Tip data for database creation
-var tipArray = [];
-
-for (var key in tip) {
-  tipArray.push(key);
-};
