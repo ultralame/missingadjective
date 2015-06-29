b3f6399860bc99a257d48e09e7ecdb0c36238486
Leon Yip <lyip1992@gmail.com>
2015-06-29 16:23:04 -0700

    implemented working grunt file

---
+++ Gruntfile.js
@@ -0,0 +1,121 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['public/client/init.js', 'public/client/*'],
+        dest: 'public/dist/<%= pkg.name %>.js'
+      }
+    },
+
+    nodemon: {
+      dev: {
+        script: 'index.js'
+      }
+    },
+
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'public/dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+
+    jshint: {
+      files: [
+        'Gruntfile.js',
+        'public/**/*.js',
+        'server/**/*.js',
+        './*.js',
+      ],
+      options: {
+        force: 'true',
+        jshintrc: '.jshintrc',
+        ignores: [
+          'public/index.html',
+          'public/lib/**/*.js',
+          'public/dist/**/*.js'
+        ]
+      }
+    },
+
+    watch: {
+      scripts: {
+        files: [
+          'public/client/**/*.js',
+          'public/lib/**/*.js',
+        ],
+        tasks: [
+          'concat',
+          'uglify'
+        ]
+      }
+    },
+
+    shell: {
+      prodServer: {
+        command: 'git push heroku master',
+        options: {
+          stdout: true,
+          stderr: true,
+          failOnError: true
+        }
+      }
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-nodemon');
+
+  grunt.registerTask('server-dev', function (target) {
+    // Running nodejs in a different process and displaying output on the main console
+    var nodemon = grunt.util.spawn({
+         cmd: 'grunt',
+         grunt: true,
+         args: 'nodemon'
+    });
+    nodemon.stdout.pipe(process.stdout);
+    nodemon.stderr.pipe(process.stderr);
+
+    grunt.task.run([ 'watch' ]);
+  });
+
+  ////////////////////////////////////////////////////
+  // Main grunt tasks
+  ////////////////////////////////////////////////////
+
+  grunt.registerTask('test', [ 'jshint' ]);
+
+  grunt.registerTask('build', [
+    'concat',
+    'uglify',
+  ]);
+
+  grunt.registerTask('upload', function(n) {
+    if(grunt.option('prod')) {
+      grunt.task.run([ 'shell:prodServer' ]);
+    } else {
+      grunt.task.run([ 'server-dev' ]);
+    }
+  });
+
+  grunt.registerTask('deploy', [
+    'test',
+    'build',
+    'upload'
+  ]);
+
+};
