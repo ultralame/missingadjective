efd1047e11e058c5aac9f8b62baf0e9a2c60c41e
Shanan Sussman <gfxbox@gmail.com>
2015-06-29 11:37:55 -0700

    refactored some canvas code for readability and added initial comments on most client files

--- client/base.js
+++ client/base.js
@@ -1,9 +1,17 @@
+/* 
+This class constructor creates each team's base. 
+Position, team ID, and radius are defined server side and passed to players upon joining the game.
+Team Id is either 0 or 1
+Position is an Object with x and y values eg: {x: 50, y:230}
+canvasContext refers to global canvas node
+*/
 var Base = function(position, canvasContext, team, radius){
   this.canvasContext = canvasContext;
   this.position = {x: position.x, y: position.y}
   this.radius = radius || 50;
   this.team = team;
-  if(this.team === 0) {
+
+  if(this.team === 0) { //handling red or blue team sides
     this.color = "rgb(0,0,200)";
   }
   else {
@@ -13,11 +21,11 @@ var Base = function(position, canvasContext, team, radius){
 
 Base.prototype.draw = function(){
   this.canvasContext.strokeStyle = this.color;   // Set the color
-  var path = new Path2D();   // Draw a circle
-  path.arc(this.position.x, this.position.y, this.radius, 0, 2*Math.PI, false);
+  var path = new Path2D();  
+  path.arc(this.position.x, this.position.y, this.radius, 0, 2*Math.PI, false); // Draws the outer circle
   this.canvasContext.stroke(path);
 
   path = new Path2D();
-  path.arc(this.position.x, this.position.y, this.radius / 5, 2 * Math.PI, false);
+  path.arc(this.position.x, this.position.y, this.radius / 5, 2 * Math.PI, false); // Draws the inner circle
   this.canvasContext.stroke(path);
 };
--- client/flag.js
+++ client/flag.js
@@ -1,38 +1,64 @@
+/*
+Constructor object and prototype functions for flag.
+Position, and radius are defined server side and passed to players upon joining the game.
+Position is an Object with x and y values eg: {x: 50, y:230}
+canvasContext refers to global canvas node
+*/
 var Flag = function(position, canvasContext, radius){
   this.position = {x: position.x, y: position.y};
   this.radius = radius || 2;
-  this.player = null;
+  this.player = null; // this value used to reference which player captures flag
   this.canvasContext = canvasContext;
   this.poleColor = "black";
   this.flagColor = "red";
-  this.dropped = false;
+  this.dropped = false; //use to prevent players immediately picking back up flag after being dropped
 };
 
+/*
+Attaches flag to player after successfull collision detection of player to flag
+player.hasFlag value used for scoring conditions
+Setting this.player tracks the current players position to attach it to them and updates its new
+position to the server
+*/
 Flag.prototype.capturedByPlayer = function(player){
   player.hasFlag = true;
   this.player = player;
 };
 
+/*
+This is only called after a player and enemy collide after the player is carrying the flag.
+Resetting player to null maintains the flag in a fixed position on the gameboard
+*/
 Flag.prototype.drop = function(){
   this.player = null;
   this.dropped = true;
   this.dropTimer();
 };
 
+/* 
+Initilizes a period of which the flag is unable to be picked up by the player that dropped it
+This allows the other team a chance to recapture the flag for their own team
+Currently hard coded to 500ms
+*/
 Flag.prototype.dropTimer = function(){
   setTimeout(function(){
     this.dropped = false;
   }.bind(this), 500);
 };
 
+/*
+Flag's position is updated every render cycle only if a player has picked it up.
+*/
 Flag.prototype.update = function(){
   if(this.player){
-    this.position.x = this.player.position.x; //change it to "x" for circle
-    this.position.y = this.player.position.y; //change it to "y" for circle
+    this.position.x = this.player.position.x;
+    this.position.y = this.player.position.y;
   };
 };
 
-
+/*
+Draws the flag shape
+*/
 Flag.prototype.draw = function(){
   // Drawing the flag pole
   this.canvasContext.fillStyle = this.poleColor; // Set color
@@ -40,7 +66,7 @@ Flag.prototype.draw = function(){
 
   // Drawing the flag
   this.canvasContext.fillStyle = this.flagColor; // Set color
-  var path = new Path2D(); // Draw triangle
+  var path = new Path2D(); // draws the triangle path
   path.lineTo(this.position.x + 5, this.position.y - 20);
   path.lineTo(this.position.x + 15, this.position.y - 15);
   path.lineTo(this.position.x + 5, this.position.y - 10);
