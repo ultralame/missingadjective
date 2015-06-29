705036b6e5d0f0e65f247c68896dd45e79553f55
Leon Yip <lyip1992@gmail.com>
2015-06-29 10:42:27 -0700

    refactored comments for keybindings

--- client/keybindings.js
+++ client/keybindings.js
@@ -1,61 +1,44 @@
 $(document).ready(function(){
-  //listening for arrow keys being pressed
-  $(document).keydown(function(event){
-    if(event.keyCode === 37) {
-      // left
-
+  $(document).keydown(function(event){  //listening for arrow keys being pressed
+    if(event.keyCode === 37) {  // left
       if(keysPressedArr.indexOf("left") === -1){
         keysPressedArr.push("left");
       }
-
     }
-    else if (event.keyCode === 38){
-      // up
-
+    else if (event.keyCode === 38){  // up
       if(keysPressedArr.indexOf("up") === -1){
         keysPressedArr.push("up");
       }
     }
-    else if (event.keyCode === 39){
-      // right
+    else if (event.keyCode === 39){  // right
       if(keysPressedArr.indexOf("right") === -1){
         keysPressedArr.push("right");
       }
     }
-    else if (event.keyCode === 40){
-      // down
+    else if (event.keyCode === 40){  // down
       if(keysPressedArr.indexOf("down") === -1){
         keysPressedArr.push("down");
       }
     }
   });
 
-  //listening for arrow keys being released
-  $(document).keyup(function(event){
-    if(event.keyCode === 37) {
-      // left
-
+  $(document).keyup(function(event){  //listening for arrow keys being released
+    if(event.keyCode === 37) {  // left
       if(keysPressedArr.indexOf("left") >= 0){
         keysPressedArr.splice(keysPressedArr.indexOf("left"), 1);
       }
     }
-    else if (event.keyCode === 38){
-      // up
-
+    else if (event.keyCode === 38){  // up
       if(keysPressedArr.indexOf("up") >= 0){
         keysPressedArr.splice(keysPressedArr.indexOf("up"), 1);
       }
     }
-    else if (event.keyCode === 39){
-      // right
-
+    else if (event.keyCode === 39){  // right
       if(keysPressedArr.indexOf("right") >= 0){
         keysPressedArr.splice(keysPressedArr.indexOf("right"), 1);
       }
     }
-    else if (event.keyCode === 40){
-      // down
-
+    else if (event.keyCode === 40){  // down
       if(keysPressedArr.indexOf("down") >= 0){
         keysPressedArr.splice(keysPressedArr.indexOf("down"), 1);
       }
