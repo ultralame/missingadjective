30670a5b46125e17f6c40342b37762a67a27dfa0
Timmy Luong <tcl.luong@gmail.com>
2015-07-01 17:29:43 -0700

    Add texturing - but inactive by default

--- public/client/base.js
+++ public/client/base.js
@@ -21,6 +21,21 @@ var Base = function(position, canvasContext, team, radius){
 var createBaseModel = function(teamId){
   var base = new THREE.BoxGeometry( 30, 30, 30 );
   var material;
+
+  // USED FOR TEXTURING
+  // var logoTexture = THREE.ImageUtils.loadTexture( 'assets/HR_logo.png' );
+  // logoTexture.minFilter = THREE.LinearFilter;
+  // logoTexture.wrapS = logoTexture.wrapT = THREE.RepeatWrapping;
+  // logoTexture.wrapS = logoTexture.wrapT = THREE.ClampToEdgeWrapping;
+  // logoTexture.anisotropy = 16;
+
+  // TEXTURING:
+  // var red = new THREE.MeshBasicMaterial( { color: 0xffffff, specular: 0xffffff, vertexColors: THREE.VertexColors,
+  //                                          map: logoTexture } );
+  // var blue = new THREE.MeshBasicMaterial({ color: 0xffffff, specular: 0xffffff, shading: THREE.FlatShading, vertexColors: THREE.VertexColors,
+  //                                          emissive: 0x111111, shininess: 10, map: logoTexture} );
+
+
   if(teamId === 1){
     material = new THREE.MeshPhongMaterial( { color: 0xff0000, specular: 0xffffff, shading: THREE.FlatShading, vertexColors: THREE.VertexColors } );
   }else{
