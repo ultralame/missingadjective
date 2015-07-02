3542ef82df899b5d0805dbc549b05b16d037ab58
mikemsrk <min2033@gmail.com>
2015-07-02 16:17:31 -0700

    collidercheck after update from server.

--- public/client/socketclient.js
+++ public/client/socketclient.js
@@ -42,10 +42,10 @@ socket.on('newPlayer', function(data){ // listening for new player creation even
 
 socket.on('broadcastPlayerPosition', function(data){ // listening for all updated player positions from the server
   var playerMovement = JSON.parse(data);
-  Collisions.playersDetection(); // checks to see if players have collided.
   envVariables.playerContainer[playerMovement.id].position = playerMovement.position;
   envVariables.playerContainer[playerMovement.id].model.position.x = playerMovement.position.x;
   envVariables.playerContainer[playerMovement.id].model.position.z = playerMovement.position.y;
+  Collisions.playersDetection(); // checks to see if players have collided.
 });
 
 socket.on('broadcastFlagPosition', function(data) { // listening for updated flag position from the server
